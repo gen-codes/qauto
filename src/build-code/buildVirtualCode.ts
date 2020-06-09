35c68fb1e6acf3936cfb993b581188c583f8c70d
Vasilis Kefallinos <vkefallinos@gmail.com>
2020-06-09 18:36:10 +0300

    feat: add rereduce functionality

--- src/build-code/buildVirtualCode.ts
+++ src/build-code/buildVirtualCode.ts
@@ -1,11 +1,15 @@
 import { buildStepLines } from './buildStepLines';
 import { Step } from '../types';
 import { VirtualCode } from './VirtualCode';
-
+import * as actions from '../actions'
 export const buildVirtualCode = (steps: Step[]): VirtualCode => {
   let previous: Step = null;
   const lines: string[] = [];
-
+  Object.values(actions).forEach(action=>{
+    if(action.rereduce){
+      steps = action.rereduce(steps)
+    }
+  })
   steps.forEach(step => {
     lines.push(...buildStepLines(step, previous));
     previous = step;
